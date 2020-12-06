f95828aa12d4d3032392aef27575a71033a5005b
r3nanp <renanmol87@gmail.com>
2020-12-06 19:39:43 -0300

    refactor: add autosuggestions commands

--- settings/profile.ps1
+++ settings/profile.ps1
@@ -1,3 +1,4 @@
+# Import themes and use theme star from oh my posh
 Import-Module posh-git
 Import-Module oh-my-posh
 Set-Theme Star
@@ -6,4 +7,8 @@ Set-Theme Star
 Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
 Set-PSReadLineKeyHandler -Key UpArrow -Function HistorySearchBackward
 Set-PSReadLineKeyHandler -Key DownArrow -Function HistorySearchForward
-# Set-Location C:\\Users\\<Username>
+Set-PSReadlineOption -HistorySearchCursorMovesToEnd
+
+# Autocomplete and autosuggestions with PSReadline
+Set-PSReadlineOption -ShowToolTips
+Set-PSReadlineOption -PredictionSource History
