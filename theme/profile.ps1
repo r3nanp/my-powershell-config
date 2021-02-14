4e4feebc6d717c36373578a5cee4782c42513298
r3nanp <renanmol87@gmail.com>
2021-02-14 10:56:07 -0300

    feat: add dynamic color

--- theme/profile.ps1
+++ theme/profile.ps1
@@ -1,14 +1,26 @@
-# Import themes and use theme star from oh my posh
 Import-Module posh-git
 Import-Module oh-my-posh
 Set-Theme Star
 
-# Autocomplete and history 
 Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
 Set-PSReadLineKeyHandler -Key UpArrow -Function HistorySearchBackward
 Set-PSReadLineKeyHandler -Key DownArrow -Function HistorySearchForward
 Set-PSReadlineOption -HistorySearchCursorMovesToEnd
 
-# Autosuggestions with PSReadline
 Set-PSReadlineOption -ShowToolTips
 Set-PSReadlineOption -PredictionSource History
+
+Import-Module Get-ChildItemColor
+
+if (-Not (Test-Path Variable:PSise)) {
+  Import-Module Get-ChildItemColor
+
+  Set-Alias l Get-ChildItem -option AllScope
+  Set-Alias ls Get-ChildItemColorFormatWide -option AllScope
+}
+
+# Chocolatey profile
+$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+if (Test-Path($ChocolateyProfile)) {
+  Import-Module "$ChocolateyProfile"
+}
